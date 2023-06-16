b2b4af47b590d5901a7e078df1ab8739c36cb48b
Younessbaalla <unessbaalla@gmail.com>
2023-06-16 09:19:01 -0700

    commiting

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,23 +9,23 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
-    {
-        printf("%d is %s\n", n, "positive");
-    }
-    else if (n < 0)
-    {
-        printf("%d is %s\n", n, "negative");
-    }
-    else
-    {
-        printf("%d is %s\n", n, "zero");
-    }
+	if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
 
-    return (0);
+	return (0);
 }
