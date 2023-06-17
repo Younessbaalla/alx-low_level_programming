fdf8a05463d2372db33a2bc6b1098fcf296d8fe2
Younessbaalla <unessbaalla@gmail.com>
2023-06-16 17:07:37 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 /**
- * main -  Determines if  the alphabet in lowercase or in uppercase
+ * main - Entry the alphabet.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
  */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	int i;
 
-int main(void) {
-
-    char low;
-    char uppercase;
-
-    for (low = 'a'; low <= 'z'; low++) {
-        putchar(low);
-    }
-    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++) {
-        putchar(uppercase);
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (i = 0; i < 52; i++)
+	{
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
 }
