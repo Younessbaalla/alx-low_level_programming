1b91f76e4cdadaa86c5f0fcc8619642d587c6a9b
Younessbaalla <unessbaalla@gmail.com>
2023-06-22 15:42:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ * 
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+		_putchar(j);
+
+		_putchar('\n');
+}
