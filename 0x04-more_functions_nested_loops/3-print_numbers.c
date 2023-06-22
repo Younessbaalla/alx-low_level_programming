1541a38de330ac73f59f0c9ef47aa017cfb7bf9e
Younessbaalla <unessbaalla@gmail.com>
2023-06-22 15:47:28 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9.
- * 
- * Return: void
  */
 
 void print_numbers(void)
@@ -11,7 +9,7 @@ void print_numbers(void)
 	int j;
 
 	for (j = 0; j <= 9; j++)
-		_putchar(j);
+		_putchar(j + '0');
 
 		_putchar('\n');
 }
