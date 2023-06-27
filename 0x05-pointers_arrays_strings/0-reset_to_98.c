bac3597a2b14a87b485b3d1687e5700ab521bee1
Younessbaalla <unessbaalla@gmail.com>
2023-06-26 18:05:22 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int and updates the value to 98.
+ * @n: Pointer to an int.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
