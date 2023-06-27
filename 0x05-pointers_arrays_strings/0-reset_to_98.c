282a716262d1e0ba961833bd56f2a694ab47ac08
Younessbaalla <unessbaalla@gmail.com>
2023-06-26 18:07:43 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - A function that takes a pointer to an int and updates the value to 98.
+ * reset_to_98 - A function that  updates the value to 98.
  * @n: Pointer to an int.
  */
 
