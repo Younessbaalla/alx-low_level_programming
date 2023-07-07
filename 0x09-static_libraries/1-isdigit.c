2e674c60f8dec9f3e2254f9e731628e49db93d0f
Younessbaalla <unessbaalla@gmail.com>
2023-07-07 15:36:08 -0700

    commit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @dest: input value
+ * @c: int to be checked
+ * 
+ * Return: 1 if c is digit ; otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
