79919e7d7ed0fdf329764aea42baeb9238f9439a
Younessbaalla <unessbaalla@gmail.com>
2023-07-10 05:15:35 -0700

    create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * create_array - create array and assign chat c
+ * @size: size of array
+ * @c: char to assign
+ * Return: pointer to array. null if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
