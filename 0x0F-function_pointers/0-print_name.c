3218816a6ec3f3b109edda59cec127b8914c3030
Younessbaalla <unessbaalla@gmail.com>
2023-07-19 05:23:21 -0700

    creat print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+/**
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+
+		f(name);
+}
+
