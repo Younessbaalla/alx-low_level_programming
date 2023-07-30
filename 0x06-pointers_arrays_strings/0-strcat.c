9f9ac11f59b875dad4e540e9f4b3990ee8affa5f
Younessbaalla <unessbaalla@gmail.com>
2023-07-30 05:20:49 -0700

    edit_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	while (*dest != '\0')
+	{
+		dest++;
+	}
 	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
 		src++;
-	}
-	while (*dest != '\0')
-	{
-		dest++;
 	}
 	*dest = '\0';
 	return (dest);
